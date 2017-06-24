a1c187c749a3f280181a6df8f13585c259304bcd
Dale <dlemke@users.noreply.github.com>
2017-06-24 16:27:36 -0500

    add login component

--- src/app/users/user.module.ts
+++ src/app/users/user.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
 import { userRouting } from './user.routing';
 
 import { ProfileComponent } from './profile/profile.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule,
     userRouting
   ],
-  declarations: [ProfileComponent],
+  declarations: [
+    ProfileComponent,
+    LoginComponent
+  ],
   providers: []
 })
 export class UserModule { }
--- src/app/users/user.routing.ts
+++ src/app/users/user.routing.ts
@@ -2,11 +2,16 @@ import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { ProfileComponent } from './profile/profile.component';
+import { LoginComponent } from './login/login.component';
 
 const userRoutes: Routes = [
     {
         path: 'profile',
         component: ProfileComponent
+    },
+    {
+        path: 'login',
+        component: LoginComponent
     }
 ];
 
