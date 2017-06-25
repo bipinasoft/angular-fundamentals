a3f8c4a84773eb6680d34b1fce99d7443cef4420
Dale <dlemke@users.noreply.github.com>
2017-06-25 16:55:59 -0500

    profile: reactive form validation

--- src/app/users/profile/profile.component.ts
+++ src/app/users/profile/profile.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { AuthenticationService } from '../../services/authentication.service';
 import { Router } from '@angular/router';
 
@@ -10,26 +10,40 @@ import { Router } from '@angular/router';
 })
 export class ProfileComponent implements OnInit {
   profileForm: FormGroup;
+  private firstName: FormControl;
+  private lastName: FormControl;
 
   constructor(private authService: AuthenticationService, private router: Router) { }
 
   ngOnInit() {
-    let firstName = new FormControl(this.authService.currentUser.firstName);
-    let lastName = new FormControl(this.authService.currentUser.lastName);
+    this.firstName = new FormControl(this.authService.currentUser.firstName, Validators.required);
+    this.lastName = new FormControl(this.authService.currentUser.lastName, Validators.required);
 
     this.profileForm = new FormGroup({
-      firstName: firstName,
-      lastName: lastName
+      firstName: this.firstName,
+      lastName: this.lastName
     });
   }
 
   saveProfile(formValues) {
-    this.authService.updateCurrentUser(formValues.firstName, formValues.lastName);
-    this.router.navigate(['list']);
+
+    if (this.profileForm.valid) {
+      this.authService.updateCurrentUser(formValues.firstName, formValues.lastName);
+      this.router.navigate(['list']);
+    }
+
   }
 
   cancel() {
     this.router.navigate(['list']);
+  }
+
+  validateFirstName(): boolean {
+    return this.firstName.valid || this.firstName.untouched;
+  }
+
+  validateLastName(): boolean {
+    return this.lastName.valid || this.lastName.untouched;
   }
 
 }
