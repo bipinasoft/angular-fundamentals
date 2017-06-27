282b27d5a93606835186808090a40cbe69bed3a3
Dale <dlemke@users.noreply.github.com>
2017-06-27 14:57:41 -0500

    added moment js

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,7 +1,9 @@
+///<reference path="../../../node_modules/moment/moment.d.ts" />
 import { Component, OnInit } from '@angular/core';
 import { EventService } from '../services/event.service';
 import { ActivatedRoute } from '@angular/router';
 import { IEventModel } from '../models/IEventModel';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-list',
@@ -9,11 +11,13 @@ import { IEventModel } from '../models/IEventModel';
   styleUrls: ['./list.component.css']
 })
 export class ListComponent implements OnInit {
+  now: any;
   events: IEventModel;
 
   constructor(private eventService: EventService, private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.now = moment().format('LLLL');
     this.events = this.route.snapshot.data['events'];
   }
 }
