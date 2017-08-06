5c257343a157f4f1cae13690fd69068074948e9c
Dale <dlemke@users.noreply.github.com>
2017-08-05 19:14:25 -0500

    persist voter service data

--- src/app/providers/voter.service.ts
+++ src/app/providers/voter.service.ts
@@ -1,23 +1,26 @@
 import { Injectable } from '@angular/core';
+import { EventService } from './event.service';
 import { ISessionModel } from '../models/ISessionModel';
 
 @Injectable()
 export class VoterService {
 
-  constructor() { }
+  constructor(private eventService: EventService) { }
 
   deleteVote(session: ISessionModel, userName: string): void {
     let voters: string[] = session.voters.split(",");
     voters = voters.filter(voter => voter !== userName);
     session.voters = voters.join();
+
+    this.eventService.saveSession(session).subscribe();
   }
 
   addVote(session: ISessionModel, userName: string): void {
     let voters: string[] = session.voters.split(",");
-
     voters.push(userName);
-
     session.voters = voters.join();
+
+    this.eventService.saveSession(session).subscribe();
   }
 
   userHasVoted(session: ISessionModel, userName: string): boolean {
